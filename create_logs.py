69c1fe5d4014daf8af3ef91b76f62b3ef58a2d2e
Selhar <selhar@protonmail.com>
2018-11-26 11:42:13 -0300

    vm

--- create_logs.py
+++ create_logs.py
@@ -9,27 +9,30 @@ def retorna_query(query):
     return c.fetchall()
     con.close()
 
-artigos_mais_populares = """
+def gera_relatorio()
+  artigos_mais_populares = """
     SELECT title as artigo, count(*) AS acessos
     FROM log, articles
     WHERE replace(log.path, '/article/', '') = articles.slug
     AND log.status LIKE '200%'
     GROUP BY title, path
     ORDER BY acessos desc limit 3;"""
-autores_mais_populares = """
+
+  autores_mais_populares = """
     SELECT name as autor, title as artigo, count(*) AS acessos
     FROM log, articles, authors
     WHERE articles.author = authors.id
-      AND replace(log.path, '/article/', '') = articles.slug
-      AND log.status LIKE '200%'
+    AND replace(log.path, '/article/', '') = articles.slug
+    AND log.status LIKE '200%'
     GROUP BY title, path, autor
     ORDER BY acessos desc limit 3;"""
-dias_com_erros_altos = """
-    SELECT * from dias_acima_de_um_porcento;
-"""
 
-output = open("relatorio.txt", "w")
+  dias_com_erros_altos = "SELECT * from dias_acima_de_um_porcento;"
 
-print(retorna_query(dias_com_erros_altos));
-output.write("teste")
-output.close()
+  queries = [artigos_mais_populares, autores_mais_populares, dias_com_erros_altos]
+  output = open("relatorio.txt", "w")
+  for query in queries
+    output.write("teste\n")
+  output.close()
+
+gera_relatorio()
