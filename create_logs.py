24ae363e8ae2c5a9c5a9de15fac58076b6144ba4
Selhar <selhar@protonmail.com>
2018-11-26 09:58:36 -0300

    queries

---
+++ create_logs.py
@@ -0,0 +1,23 @@
+# 1. Quais são os três artigos mais populares de todos os tempos?
+# SELECT title as artigo, count(*) AS acessos
+# FROM log, articles
+# WHERE replace(log.path, '/article/', '') = articles.slug AND log.status LIKE '200%'
+# GROUP BY title, path
+# ORDER BY acessos desc limit 3;
+
+# 2. Quem são os autores de artigos mais populares de todos os tempos?
+# SELECT name as autor, title as artigo, count(*) AS acessos
+# FROM log, articles, authors
+# WHERE articles.author = authors.id
+#   AND replace(log.path, '/article/', '') = articles.slug
+#   AND log.status LIKE '200%'
+# GROUP BY title, path, autor
+# ORDER BY acessos desc limit 3;
+
+# 3. Em quais dias mais de 1% das requisições resultaram em erros?
+
+SELECT date(time) as dia, count(*) as erros
+FROM log
+WHERE status NOT LIKE '200%'
+GROUP BY dia
+ORDER BY erros desc;
