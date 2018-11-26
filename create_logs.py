41c28f5d2cad5b905d275ede28a46525ec51f3e1
Selhar <selhar@protonmail.com>
2018-11-26 10:44:44 -0300

    queries

--- create_logs.py
+++ create_logs.py
@@ -18,6 +18,7 @@
 # 3. Em quais dias mais de 1% das requisições resultaram em erros?
 
 erros
+# CREATE VIEW erros AS
 # SELECT date(time) as dia, count(*) as erros
 # FROM log
 # WHERE status NOT LIKE '200%'
@@ -25,13 +26,22 @@ erros
 # ORDER BY erros desc;
 
 total
+# CREATE VIEW total AS
 # SELECT date(time) as dia, count(*) as total
 # FROM log
 # GROUP BY dia
-# ORDER BY erros desc;
+# ORDER BY total desc;
 
-%
-# SELECT erros, total, dia
+% per day
+# CREATE VIEW threshold AS
+# SELECT total.dia as dia, (total.total / 100) as um_porcento_do_dia
 # FROM total
-# GROUP BY dia
-# ORDER BY erros desc;
+# ORDER BY um_porcento_do_dia desc;
+
+resultado
+# CREATE VIEW dias_acima_de_um_porcento as
+# SELECT erros.dia, erros.erros
+# FROM erros, threshold
+# WHERE erros.dia = threshold.dia
+#   AND erros.erros > threshold.um_porcento_do_dia
+# ORDER BY erros.dia desc;
